2aac73c7fcbc18937f76030986ee352f96f16693
Öznur Kandakoğlu <oznurkandakoglu@gmail.com>
2023-04-26 16:35:53 +0300

    Initial Commit

---
+++ SwiftUIDatas/SwiftUIDatasApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDatasApp.swift
+//  SwiftUIDatas
+//
+//  Created by Tiga  on 26.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDatasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
